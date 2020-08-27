c39dc630690b817acd6104194ef07dae1c69de0b
BoatYuma <66005374+BoatYuma@users.noreply.github.com>
2020-08-27 13:32:40 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        
         ColectieGreutati colectie1 = new ColectieGreutati();
 
+        //greutate simpla
         Simple greutate1 = new Simple(23);
         colectie1.adauga(greutate1);
 
+        //greutate dubla
         Greutati greutate21 = new Greutati(34);
         Greutati greutate22 = new Greutati(65);
         Duble greutate2 = new Duble(greutate21, greutate22);
         colectie1.adauga(greutate2);
 
+        //greutate multipla
         Greutati[] greutateA = new Greutati[3];
         greutateA[0] = greutate1;
         greutateA[1] = greutate2;
@@ -21,6 +24,6 @@ public class Main {
         Multiple greutate3 = new Multiple(greutateA);
         colectie1.adauga(greutate3);
 
-        System.out.println("Medie: "+colectie1.medie());
+        System.out.println("Medie: " + colectie1.medie());
     }
 }
